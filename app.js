6a19739d7bb500c2d5e5ea67c320b75bd15d2625
ashishnhhh <ashishnhansbhavi@gmail.com>
2023-04-11 21:45:46 +0530

    consant

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+const info_routes = require("./router/infoR");
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/",(res,req) =>{
-    res.send('hi');
+    res.send('hi server alive');
 });
 
+
+app.use("/info", info_routes);
 const start = async() =>{
     try{
         app.listen(PORT, () => {
