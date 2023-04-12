de74bc44b874bf18b7a0f96acf384738d355db90
ashishnhhh <130228500+ashishnhhh@users.noreply.github.com>
2023-04-11 22:21:36 -0700

    Delete fire.js

--- fire.js
+++
@@ -1,9 +0,0 @@
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://aashish123-18385-default-rtdb.asia-southeast1.firebasedatabase.app"
-});
